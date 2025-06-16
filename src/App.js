3c62644924f5ea993c177b80b554395102d9cb60
Merel Jacobs <merel.burleigh@gmail.com>
2025-06-16 07:55:11 -0700

    background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import FAQ from "./components/FAQ";
 import Header from "./components/Header";
@@ -7,40 +7,20 @@ import Tiles from "./components/Tiles";
 import WeddingInfo from "./components/WeddingInfo";
 
 function App() {
-  const weddingInfoRef = useRef(null);
-  const [useWhiteBackground, setUseWhiteBackground] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (!weddingInfoRef.current) return;
-      const rect = weddingInfoRef.current.getBoundingClientRect();
-      const halfway = rect.top + rect.height / 4;
-      setUseWhiteBackground(halfway < window.innerHeight / 4);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
-    <div
-      className={`relative top-0 left-0 w-full min-h-screen  duration-700 ${
-        useWhiteBackground
-          ? "bg-white"
-          : "bg-[url('./assets/wedding.jpg')] bg-cover bg-no-repeat bg-fixed"
-      }`}
-    >
-      {!useWhiteBackground && (
-        <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 z-10"></div>
-      )}
+    <div className="relative top-0 left-0 w-full min-h-screen bg-[url('./assets/wedding.jpg')] bg-cover bg-no-repeat bg-fixed">
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 z-10 pointer-events-none"></div>
 
       <div className="relative z-20">
         <Header />
-        <div className="flex justify-center" ref={weddingInfoRef}>
+        <div className="flex justify-center">
           <WeddingInfo />
         </div>
         <FAQ />
-        <MrMrs />
+        <div className="bg-white">
+          <MrMrs />
+        </div>
+
         <Tiles />
       </div>
     </div>
