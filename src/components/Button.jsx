bf7e63387ec535bd4540947a0677136bc69a6966
Merel Jacobs <merel.burleigh@gmail.com>
2025-07-25 17:35:48 -0700

    updates

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({text, href}) {
   return (
     <button className="px-4 py-2 bg-backgrounBeige rounded-2xl border-2 border-darkBeige hover:opacity-80 transition duration-300 hover:scale-105 hover:ease-in-out">
-      <a href={href} className="text-darkBeige no-underline">
+      <a href={href} target="_blank" className="text-darkBeige no-underline">
         {text}
       </a>
     </button>
--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import headshot from "../assets/headshot.png"
+
+
+import React from "react";
+import headshot from "../assets/headshot.png";
+
 function ThankYou() {
   return (
-    <div className=" bg-[url('./assets/hands.png')] bg-cover bg-center bg-no-repeat bg-fixed shadow-lg px-10 py-12  min-h-[80vh]">
-      Thanks for celebrating with us! 
-      <img src={headshot}>
-      </img>
+    <div className="bg-[url('./assets/hands.png')] bg-cover bg-center bg-no-repeat bg-fixed shadow-lg px-10 py-12 min-h-[80vh] flex items-center">
+      <img src={headshot} alt="Headshot" className=" object-contain" />
+
+      <div data-aos="fade-left" className="text-start mr-6 min-w-fit">
+        <h2 className="text-8xl font-bold text-darkBeige transform rotate-90">Thanks for celebrating with us!</h2>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ThankYou
+export default ThankYou;
