af1fe56b3e512ad95117e315678cf7f5e21aef44
Merel Jacobs <merel.burleigh@gmail.com>
2025-08-03 13:53:47 -0700

    build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Initials from "./Initals";
 
 function Header() {
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-[50vh] md:min-h-screen bg-[url('./assets/hand.png')] bg-center bg-cover bg-no-repeat bg-scroll md:bg-fixed">
+    <div className="relative flex flex-col items-center justify-center min-h-[100vh] md:min-h-screen bg-[url('./assets/hand.png')] bg-center bg-cover bg-no-repeat bg-scroll md:bg-fixed">
       <Initials />
     </div>
   );
