7ea4bf80526ad4e0d919db769e98bd69d4d05a56
Merel Jacobs <merel.burleigh@gmail.com>
2025-08-03 19:50:10 -0700

    build

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -5,7 +5,7 @@ function Buttons() {
   return (
     <div className="bg-[url('./assets/stool.png')] bg-cover bg-center bg-no-repeat bg-scroll md:bg-fixed ">
       <div data-aos="zoom-out" className="flex flex-col md:flex-row justify-center gap-10 pt-4 items-center min-h-[60vh] ">
-        <Button image={rsvp} href="#"></Button>
+        <Button image={rsvp} href="https://www.zola.com/wedding/merelandmatthew/rsvp"></Button>
         <Button image={registry} href="https://www.zola.com/registry/merelandmatthew"></Button>
       </div>
     </div>
